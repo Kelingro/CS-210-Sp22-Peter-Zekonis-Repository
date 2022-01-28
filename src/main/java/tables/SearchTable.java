d0618fe0958e071e2da4f6bfeeab27783dc765e5
Kelingro <zprunner116@gmail.com>
2022-01-28 13:04:35 -0500

    Implemented Binary Search

--- src/main/java/tables/SearchTable.java
+++ src/main/java/tables/SearchTable.java
@@ -1,5 +1,7 @@
 package tables;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -33,42 +35,61 @@ public class SearchTable extends Table {
 	public void clear() {
 		list.clear();
 	}
+	
+	private class RowKeyComparator implements Comparator<Object>{
 
-	@Override
-	public boolean put(List<Object> row) {
-		Object key = row.get(primaryIndex);
-
-		for (int i = 0; i < list.size(); i++) {
-			List<Object> old = list.get(i);
-			if (old != null && old.get(primaryIndex).equals(key)) {
-				list.set(i, row);
-				return true;
+		@Override
+		public int compare(Object o1, Object o2) {
+			
+			return keyOf(o1).compareTo(keyOf(o2));
+			
+		}
+		@SuppressWarnings("unchecked")
+		private Comparable<Object> keyOf(Object obj) {
+			if(obj instanceof List<?> row) {
+				return (Comparable<Object>)row.get(primaryIndex);
+			}
+			else {
+				return (Comparable<Object>) obj;
 			}
 		}
-		list.add(row);
-		return false;
+		
 	}
 
 	@Override
-	public boolean remove(Object key) {
-		for (int i = 0; i < list.size(); i++) {
-			List<Object> row = list.get(i);
-			if (row != null && row.get(primaryIndex).equals(key)) {
-				list.remove(i);
-				return true;
+	public boolean put(List<Object> row) {
+		int index= Collections.binarySearch(list, row, new RowKeyComparator());
+		if(index>=0) {
+			list.set(index, row);
+			return true;
+		}
+		else {
+			list.add(-index-1, row);
+			return false;
 			}
 		}
-		return false;
+
+	@Override
+	public boolean remove(Object key) {
+		int index= Collections.binarySearch(list, key,new RowKeyComparator());
+		if(index>=0) {
+			list.remove(index);
+			return true;
+		}
+		else {
+			return false;
+			}
 	}
 
 	@Override
 	public List<Object> get(Object key) {
-		for (int i = 0; i < list.size(); i++) {
-			List<Object> row = list.get(i);
-			if (row.get(primaryIndex).equals(key))
-				return row;
+		int index= Collections.binarySearch(list, key, new RowKeyComparator());
+		if(index>=0) {
+			return list.get(index);
 		}
-		return null;
+		else {
+			return null;
+			}
 	}
 
 	@Override
